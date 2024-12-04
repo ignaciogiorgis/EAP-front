a683d4fd19f8abd4f32c49be9f52a3bc2fde90c2
ignaciogiorgis <ignaciogiorgis2016@gmail.com>
2024-12-03 21:40:58 -0300

    43-Create-main-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,44 @@
+import Link from "next/link";
+import { LiaBusinessTimeSolid } from "react-icons/lia";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      App main
-    </div>
+    <>
+      <div className="relative">
+        {/* Fondo con forma de triángulo */}
+        <div
+          className="py-40 absolute inset-0 h-full w-full -z-10"
+          style={{
+            clipPath: "polygon(0 0, 100% 0, 100% 50%, 0 100%)",
+            backgroundColor: "#1f2937",
+          }}
+        ></div>
+
+        {/* Contenido superior */}
+        <div className="relative text-white py-3 px-5 flex flex-wrap justify-between">
+          <h1 className="text-4xl font-bold">
+            Expenses <span className="font-medium">and</span> Profits
+          </h1>
+          <ul className="flex gap-[40px] text-m items-center">
+            <li className="hover:text-indigo-500 cursor-pointer">
+              <Link href="/auth/login">Sign In</Link>
+            </li>
+            <li className="hover:text-indigo-500 cursor-pointer">
+              <Link href="/auth/register">Create an account</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* H2 centrado en la pantalla */}
+      <div className="flex justify-center items-center gap-10 h-screen">
+        <h2 className="text-4xl mt-6 font-bold text-indigo-950 animate-fade-move">
+          A solution to your business
+        </h2>
+        <div>
+          <LiaBusinessTimeSolid className="text-indigo-950 text-6xl mt-2  animate-bounce" />
+        </div>
+      </div>
+    </>
   );
 }
